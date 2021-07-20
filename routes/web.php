4cac598718fa3f809d0ac42b1e21234456667ba2
Nataly Chea <34535916+Cheanataly@users.noreply.github.com>
2021-07-20 15:56:50 +0700

    routers and controllers

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+*/
+
+/*
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return 'This is user '.$name.' with the id of '.$id;
+});
+*/
+
+Route::get('/', [PagesController::class, 'index']);
+
+Route::get('/about', [PagesController::class, 'about']);
+
+Route::get('/services', [PagesController::class, 'services']);
+
+
