75b49cc583d9153b79dcdf853ff700922ab5fa7c
Nataly Chea <34535916+Cheanataly@users.noreply.github.com>
2021-07-21 15:46:24 +0700

    Blade Templating & Compiling Assets

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <div class="container">
+      <a class="navbar-brand text-light" href="/">{{config('app.name', 'LSAPP')}}</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item body-bg">
+            <a class="nav-link text-light" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="/services">Services</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
+@section('content')
+    <h1>{{$title}}</h1>
+    @if(count($services) > 0)
+        <ul class="list-group">
+            @foreach($services as $service)
+                <li class="list-group-item">{{$service}}</li>
+            @endforeach
+        </ul>
+    @endif
+@endsection
 
-    </head>
-    <body class="antialiased">
-        <h1>Services</h1>
-        <p>This is the services page</p>
-    </body>
-</html>
